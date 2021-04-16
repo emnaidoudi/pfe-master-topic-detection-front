2ab01a8a1ffacae2796b7de8ea6afef5bbfda324
Emna Aidoudi <emna.aidoudi@advyteam.com>
2021-04-16 13:16:00 +0100

    hello

---
+++ src/app/cloud/cloud.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CloudData, CloudOptions } from 'angular-tag-cloud-module';
+
+@Component({
+  selector: 'app-cloud',
+  templateUrl: './cloud.component.html',
+  styleUrls: ['./cloud.component.css']
+})
+export class CloudComponent implements OnInit {
+
+  options: CloudOptions = {
+    // if width is between 0 and 1 it will be set to the width of the upper element multiplied by the value
+    width: 1000,
+    // if height is between 0 and 1 it will be set to the height of the upper element multiplied by the value
+    height: 400,
+    overflow: false,
+  };
+
+  data: CloudData[] = [
+    {text: 'Weight-8-link-color', weight: 8, link: 'https://google.com', color: '#ffaaee'},
+    {text: 'Weight-10-link', weight: 10, link: 'https://google.com', tooltip: 'display a tooltip'},
+    // ...
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
