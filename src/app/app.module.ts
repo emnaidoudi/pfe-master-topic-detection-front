0a217b5221430c1d2e5eb22ea0d52b6dc681b55d
Emna Aidoudi <emna.aidoudi@advyteam.com>
2021-04-16 13:15:14 +0100

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CloudComponent } from './cloud/cloud.component';
+import {TagCloudModule} from 'angular-tag-cloud-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CloudComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TagCloudModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
